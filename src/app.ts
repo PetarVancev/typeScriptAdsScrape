f9f7ac348f5b10f7f99cafe95610d450aeaf2f39
Petar <pvancev@gmail.com>
2022-11-02 11:53:52 +0100

    Successfully inserted data into DB

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.get("/api", (req: Request, res: Response) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import puppeteer from "puppeteer";
 
-interface propertyData {
-  title: string;
-  imgUrl: string;
-}
+const { Pool } = require("pg");
 
-(async () => {
-  for (var j = 1; j <= 25; j++) {
+const pool = new Pool({
+  user: "test_user",
+  database: "taskDB",
+  password: "Watermelon555",
+  port: 5431,
+  host: "localhost",
+});
+
+async function start() {
+  let data = [];
+  for (let j = 1; j <= 25; j++) {
     const url =
       "https://www.sreality.cz/en/search/for-sale/apartments/all-countries?page=" +
       j;
@@ -16,25 +22,47 @@ interface propertyData {
 
     await page.goto(url, { waitUntil: "networkidle2" });
 
-    const data = await page.evaluate(() => {
-      const elementsArray: propertyData[] = [];
+    const pageData = await page.evaluate(() => {
+      const titlesArray: string[] = [];
+      const urlsArray: string[] = [];
       for (var i = 0; i < 20; i++) {
         const titleElement = document.getElementsByClassName("name")[i];
         const imgElement = document.querySelectorAll(
           "[component='property-carousel'] a:first-child img"
         )[i] as HTMLImageElement | null;
-        const elementObj: propertyData = {
-          title: titleElement.textContent,
-          imgUrl: imgElement.src,
-        };
-        elementsArray.push(elementObj);
+        titlesArray.push(titleElement.textContent);
+        urlsArray.push(imgElement.src);
       }
 
-      return elementsArray;
+      return { titlesArray, urlsArray };
     });
 
-    console.log(data);
-
+    data.push(pageData);
     await browser.close();
   }
-})();
+  return data;
+}
+
+async function insertData() {
+  const fullData = await start();
+  let count = 0;
+  for (let i = 0; i < 25; i++) {
+    let pageData = fullData[i];
+    console.log(pageData);
+    for (let j = 0; j < 20; j++) {
+      count++;
+      try {
+        const res = await pool.query(
+          "INSERT INTO siteDB (id, title, url) VALUES ($1, $2, $3) ON CONFLICT (id) DO UPDATE SET title = $2, url = $3;",
+
+          [count, pageData.titlesArray[i], pageData.urlsArray[i]]
+        );
+        console.log("Succesfully added item " + i);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+  console.log("done");
+}
+insertData();
